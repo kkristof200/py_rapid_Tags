b86cab0053d4c32d8da3b007a9993b498d721862
kkristof200 <kovacskristof200@gmail.com>
2020-11-05 22:20:07 +0200

    new version

--- rapid_tags/rapid_tags.py
+++ rapid_tags/rapid_tags.py
@@ -7,7 +7,6 @@ import random, json
 
 # Pip
 from kcu import request
-import brotli
 
 # ---------------------------------------------------------------------------------------------------------------------------------------- #
 
@@ -51,11 +50,10 @@ class RapidTags:
             if type(proxy) == list:
                 proxy = random.choice(proxy) if len(proxy) > 0 else None
             
-            response = request.get(
+            return [t.replace('\\', '') for t in request.get(
                 'https://rapidtags.io/api/generator?query=' + quote(title),
                 headers={
                     'Host': 'rapidtags.io',
-                    # 'User-Agent': user_agent,
                     'Accept': 'application/json',
                     'Accept-Language': 'en-US,en;q=0.5',
                     'Accept-Encoding': 'gzip, deflate',
@@ -68,18 +66,10 @@ class RapidTags:
                     'Upgrade-Insecure-Requests': '1'
                 },
                 max_request_try_count=1,
-                # user_agent=user_agent,
+                user_agent=user_agent,
                 proxy=proxy,
                 debug=debug
-            )
-
-            try:
-                j = response.json()
-            except:
-                pass
-                # j = json.loads(brotli.decompress(response.content))
-
-            return [t.replace('\\', '') for t in j['tags']]
+            ).json()['tags']]
         except Exception as e:
             if debug:
                 print(e)
--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ else:
 
 setuptools.setup(
     name="rapid_tags",
-    version="0.0.4",
+    version="0.0.5",
     author="Kristof",
     description="python wrapper for rapidtags.io",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/kkristof200/py_rapid_Tags",
     packages=setuptools.find_packages(),
-    install_requires=["Brotli", "kcu"],
+    install_requires=["kcu"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
